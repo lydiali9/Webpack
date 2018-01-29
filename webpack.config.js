388ef9eb393bd8deca460a2e4b953d75012b46a3
Lynn Li <yuhua.li@inveno.com>
2018-01-29 18:02:46 +0800

    [feature]: organize files and folders

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
+var path = require("path");
+
 module.exports = {
+    context: path.resolve('js'),
     entry: ["./utils", "./app.js"],
     output: {
+        path: path.resolve('build/js/'),
+        publicPath: '/public/assets/js/',
         filename: "bundle.js"
     },
     watch: true,
+
+    devServer: {
+        contentBase: 'public'
+    },
 
     module: {
         loaders: [
